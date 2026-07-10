6211c9a6783f50bf1f0b3409ee13bea06c7db782
MAHESHISAGARIKA <maheshi.sagarika002@gmail.com>
2026-07-10 10:37:05 +0530

    Initial Version and ProductCard creation✏️

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+
+  return (
+    <>
+      <ProductCard name="Macbook Air" price="LKR.250,000" photo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOVMOypaOTrQGqBYDi2UoUJORrwmQNawDvgw3VBa2EiA&s=10"/>
+      <ProductCard name="Apple iphone 4s" price="LKR.50,000" photo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQrLavn16XrKalJVxu7Pzm9iZQIItyzTyhRQW2EM-pJw&s=10"/>
+    </>
+  )
+}
+
+export default App
