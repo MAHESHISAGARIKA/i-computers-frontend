85b8beb41c45bce00a822d8ebe1d3cbcd44236d7
MAHESHISAGARIKA <maheshi.sagarika002@gmail.com>
2026-07-17 23:42:10 +0530

    Modify admin page and  create adminProductsPage

---
+++ src/pages/admin/adminProductPage.jsx
@@ -0,0 +1,15 @@
+import { FaPlus } from "react-icons/fa6";
+
+export default function AdminProductsPage(){
+
+    return(
+        <div className="w-full h-full">
+            
+             <button className="bg-accent w-[80px] h-[80px] rounded-full text-white text-2xl flex justify-center items-center fixed bottom-4 right-4 shadow-2xl hover:bg-white hover:text-accent">
+                < FaPlus/>
+
+             </button>
+        </div>
+    )
+
+}
--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,26 +1,39 @@
-import { Routes, Route,Link } from "react-router";
+import { Routes, Route, Link } from "react-router";
+import { FiShoppingCart } from "react-icons/fi";
+import { GrGift } from "react-icons/gr";
+import { HiOutlineUsers } from "react-icons/hi2";
+import AdminProductsPage from "./admin/adminProductPage";
 
 export default function AdminPage() {
   return (
-    <div className="w-full h-full bg-purple-500 flex">
-      <div className="w-[300px] h-full bg-white flex flex-col">
-        <h1 className="w-full h-[50px] bg-gray-400 text-center text-xl font-bold">Using Anchor Tags</h1>
-           <a className="w-[100px] h-[50px] bg-blue-500 my-3" href="/admin/">Orders</a>
-           <a className="w-[100px] h-[50px] bg-blue-500 my-3" href="/admin/products">Products</a>
-           <a className="w-[100px] h-[50px] bg-blue-500 my-3" href="/admin/users">Users</a>
+    <div className="w-full h-full bg-primary flex">
+      <div className="w-[300px] h-full bg-white flex flex-col shadow-2xl">
+        <div className="w-full h-[100px] py-4 px-2">
+          <img src="/logo.png" className="h-full" />
+        </div>
 
-        <h1 className="w-full h-[50px] bg-gray-400 text-center text-xl font-bold">Using link component</h1>
-           <Link className="w-[100px] h-[50px] bg-blue-500 my-3" to="/admin/">Orders</Link>
-           <Link className="w-[100px] h-[50px] bg-blue-500 my-3" to="/admin/products">Products</Link>
-           <Link className="w-[100px] h-[50px] bg-blue-500 my-3" to="/admin/users">Users</Link> 
-    
+        <Link to="/admin" className="w-full p-4 text-xl text-gray-500 flex items-center gap-4">
+          <FiShoppingCart />
+          <span className="w-full h-full block py-2">Orders</span>
+        </Link>
+
+        <Link to="/admin/products" className="w-full p-4 text-xl text-gray-500 flex items-center gap-4" >
+          <GrGift />
+          <span className="w-full h-full block py-2">Products</span>
+        </Link>
+
+        <Link to="/admin/users" className="w-full p-4 text-xl text-gray-500 flex items-center gap-4">
+          <HiOutlineUsers />
+          <span className="w-full h-full block py-2">Users</span>
+        </Link>
       </div>
 
-      <div className="w-[calc(100%-300px)] h-full bg-amber-200">
+      <div className="w-[calc(100%_-_300px)] h-full p-4">
         <Routes>
-          <Route path="/" element={<h1>Orders page</h1>} />
-          <Route path="/products" element={<h1>Products page</h1>} />
-          <Route path="/users" element={<h1>Users Page</h1>} />
+          <Route path="" element={<h1>Orders Page</h1>} />
+          <Route path="products" element={<AdminProductsPage />}/>
+          <Route path="users" element={<h1>Users Page</h1>}/>
+
         </Routes>
       </div>
     </div>
